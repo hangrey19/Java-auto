6a2c8185ee5d956f2a335fc041627aefe3529450
hangrey19 <huongn0903@gmail.com>
2026-01-21 11:10:01 +0700

    Done bài 1: Overview

---
+++ java-basic-automation/src/LoginInfo.java
@@ -0,0 +1,11 @@
+public class LoginInfo {
+    public static void main(String[] args) {
+        String username = "abc@gmailcom";
+        String password = "123456";
+        String expectedResult = "SUCCESS";
+
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("Expected Result: " + expectedResult);
+    }
+}
