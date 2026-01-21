0155ebe21664293822f4e8269a09cd6dd710a82f
hangrey19 <huongn0903@gmail.com>
2026-01-21 11:12:39 +0700

    Done bài 2: If-else

---
+++ java-basic-automation/src/LoginValidator.java
@@ -0,0 +1,13 @@
+public class LoginValidator {
+    public static void main(String[] args) {
+        String expected = "SUCCESS";
+        String actual = "SUCCESS";
+
+        if (actual.equals(expected)) {
+            System.out.println("TEST PASSED");
+        }
+        else {
+            System.out.println("TEST FAILED");
+        }
+    }
+}
